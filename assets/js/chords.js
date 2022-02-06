f0b0bad2e4c49ca6c5c7b6742356e989d57fa336
Nicolas Lupien <nicolas@expedibox.com>
2022-02-06 12:46:19 -0500

    update

--- assets/js/chords.js
+++ assets/js/chords.js
@@ -1,23 +1,36 @@
-
-const CHORDS = [
-  "A", "Ab", "Am", "Am7", "A#", "A#m",  "A#m7",
-  "B", "Bb", "Bm", "Bm7", "B#", "B#m",  "B#m7",
-  "C", "Cb", "Cm", "Cm7", "C#", "C#m",  "C#m7",
-  "D", "Db", "Dm", "Dm7", "D#", "D#m",  "D#m7",
-  "E", "Eb", "Em", "Em7", "E#", "E#m",  "E#m7",
-  "F", "Fb", "Fm", "Fm7", "F#", "F#m",  "F#m7",
-  "G", "Gb", "Gm", "Gm7", "G#", "G#m",  "G#m7"
- ];
+function allChords() {
+  let chords = [];
+  const notes = ["A", "B", "C", "D", "E", "F", "G"];
+  notes.forEach((note) => chords.push(note));
+  notes.forEach((note) => chords.push(note + "m"));
+  notes.forEach((note) => chords.push(note + "7"));
+  notes.forEach((note) => chords.push(note + "b"));
+  notes.forEach((note) => chords.push(note + "m7"));
+  notes.forEach((note) => chords.push(note + "#"));
+  notes.forEach((note) => chords.push(note + "#m"));
+  notes.forEach((note) => chords.push(note + "#m7"));
+  notes.forEach((note) => chords.push(note + "(sus4)"));
+  notes.forEach((note) => chords.push(note + "maj7"));
+  notes.forEach((note) => notes.forEach((note2) => chords.push(note + "/" + note2)));
+  notes.forEach((note) => notes.forEach((note2) => chords.push(note + "/" + note2 + "#")));
+  console.log(chords);
+  return chords;
+}
 
 function parseChords(text) {
-  return CHORDS.reduce((acc, chord) => {
-    text.includes(` ${chord} `) && acc.add(chord);
+  lines = text.split("\n");
+  const chordSet = allChords().reduce((acc, chord) => {
+    let some = lines.some(text => text.includes(` ${chord} `));
+    some = some || lines.some(text => text.startsWith(`${chord} `));
+    some = some || lines.some(text => text.endsWith(` ${chord}`));
+    some && acc.add(chord);
     return acc;
   }, new Set());
+  return Array.from(chordSet).sort();
 }
 
 function renderChord(chord = "") {
-  const c = chord.replace("#", "d");
+  const c = chord.replace("#", "d").replace("/", "_");
   const div = document.createElement("div");
   div.className = "d-flex flex-column text-center fw-bold";
   div.innerHTML = `<div class="ms-3">${chord}</div><img src="https://www.boiteachansons.net/images/accords/guitare/droitier/${c[0]}/${c}/${c}.gif">`
