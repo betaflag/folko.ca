6683f3c890336ccece8c9d89c2d65971b57c4481
Nicolas Lupien <nicolas@expedibox.com>
2022-02-06 13:05:17 -0500

    update

--- assets/js/chords.js
+++ assets/js/chords.js
@@ -1,29 +1,44 @@
+const NOTES = ["A", "B", "C", "D", "E", "F", "G"];
+const VARIATIONS = [
+  "m",
+  "7",
+  "b",
+  "m7",
+  "(sus4)",
+  "maj7",
+  "/A",
+  "/B",
+  "/C",
+  "/D",
+  "/E",
+  "/F",
+  "/G",
+  "/A#",
+  "/B#",
+  "/C#",
+  "/D#",
+  "/E#",
+  "/F#",
+  "/G#",
+];
+
 function allChords() {
-  let chords = [];
-  const notes = ["A", "B", "C", "D", "E", "F", "G"];
-  notes.forEach((note) => chords.push(note));
-  notes.forEach((note) => chords.push(note + "m"));
-  notes.forEach((note) => chords.push(note + "7"));
-  notes.forEach((note) => chords.push(note + "b"));
-  notes.forEach((note) => chords.push(note + "m7"));
-  notes.forEach((note) => chords.push(note + "#"));
-  notes.forEach((note) => chords.push(note + "#m"));
-  notes.forEach((note) => chords.push(note + "#m7"));
-  notes.forEach((note) => chords.push(note + "(sus4)"));
-  notes.forEach((note) => chords.push(note + "maj7"));
-  notes.forEach((note) => notes.forEach((note2) => chords.push(note + "/" + note2)));
-  notes.forEach((note) => notes.forEach((note2) => chords.push(note + "/" + note2 + "#")));
-  console.log(chords);
-  return chords;
+  return NOTES.reduce((chords, note) => {
+    chords.push(note);
+    VARIATIONS.forEach((variation) => chords.push(note + variation));
+    return chords;
+  }, []);
 }
 
 function parseChords(text) {
   lines = text.split("\n");
   const chordSet = allChords().reduce((acc, chord) => {
-    let some = lines.some(text => text.includes(` ${chord} `));
-    some = some || lines.some(text => text.startsWith(`${chord} `));
-    some = some || lines.some(text => text.endsWith(` ${chord}`));
-    some && acc.add(chord);
+    if (
+      lines.some((text) => text.includes(` ${chord} `)) ||
+      lines.some((text) => text.startsWith(`${chord} `)) ||
+      lines.some((text) => text.endsWith(` ${chord}`))
+    )
+      acc.add(chord);
     return acc;
   }, new Set());
   return Array.from(chordSet).sort();
@@ -33,10 +48,12 @@ function renderChord(chord = "") {
   const c = chord.replace("#", "d").replace("/", "_");
   const div = document.createElement("div");
   div.className = "d-flex flex-column text-center fw-bold";
-  div.innerHTML = `<div class="ms-3">${chord}</div><img src="https://www.boiteachansons.net/images/accords/guitare/droitier/${c[0]}/${c}/${c}.gif">`
+  div.innerHTML = `<div class="ms-3">${chord}</div><img src="https://www.boiteachansons.net/images/accords/guitare/droitier/${c[0]}/${c}/${c}.gif">`;
   const chordsRef = document.getElementById("chordsRef");
   chordsRef.appendChild(div);
 }
 
 let content = document.getElementById("content");
-parseChords(content.textContent).forEach(renderChord);
+if (content) {
+  parseChords(content.textContent).forEach(renderChord);
+}
